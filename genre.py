ce9404f7eb1f438d9c23e83495b97005807cf614
sona.darshan <151994881+sonadarshan99@users.noreply.github.com>
2024-07-05 16:21:10 +0530

    Create genre.py

---
+++ genre.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class GenreEnum(Enum):
+    """Types of Movie Genres"""
+    BIOGRAPHY = "BIOGRAPHY"
+    HORROR = "HORROR"
+    THRILLER = "THRILLER"
+    SUPERHERO = "SUPERHERO"
+    SCI_FI = "SCI_FI"
+    DRAMA = "DRAMA"
+    ACTION = "ACTION"
+    CRIME = "CRIME"
